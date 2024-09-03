8883f873f305e6d5119ea60bdd66efbe3c95ec03
Someswar Gorai <somgorai726@gmail.com>
2024-09-03 16:08:38 +0530

    updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,38 +6,12 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 // Styled components
-const Banner = styled(Box)({
-  width: "100vw",
-  textAlign: "center",
-  backgroundImage: "url(./images/about-us.png)",
-  backgroundRepeat: "no-repeat",
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  paddingTop: "10vw",
-  paddingBottom: "8vw",
-  position: "relative",
-  "&::before": {
-    content: '""',
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Dark overlay
-    zIndex: 1,
-  },
-});
 
-const WhyChooseSection = styled(Box)({
-  backgroundColor: "#f4f4f4",
-  padding: "40px 20px",
-  borderRadius: "8px",
-  textAlign: "center",
-  marginTop: "20px",
-});
+
+
 
 const ContentSection = styled(Box)({
-  padding: "4rem 2rem",
+  padding: "2rem 1rem",
 });
 
 const About = () => {
@@ -46,7 +20,30 @@ const About = () => {
   return (
     <Box>
       <Navbar />
-      <Banner>
+      <Box
+        sx={{
+          width: "100vw",
+          textAlign: "center",
+          backgroundImage:
+            "url(./images/about-us.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          paddingTop: "2vw",
+          paddingBottom: "15vw",
+          position: "relative",
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.4)", // Overlay with opacity
+            zIndex: 1,
+          },
+        }}
+      >
         <Grid
           container
           sx={{
@@ -56,30 +53,44 @@ const About = () => {
             padding: { xs: "20px", sm: "20px", md: "50px" },
           }}
         >
-          <Grid item xs={12}>
+          <Grid item xs={12} sm={12} lg={6} md={6}>
             <Typography
               variant="h1"
               sx={{
-                fontSize: { xs: "2rem", sm: "2.4rem", md: "3rem" },
+                fontSize: {
+                  xs: "2rem",
+                  sm: "2.4rem",
+                  md: "2.6rem",
+                  lg: "2.6rem",
+                },
+                marginTop: { xs: "20px", md: "50px" },
                 fontWeight: "bold",
-                color: "white",
+                color:"white"
               }}
             >
               About Us
             </Typography>
             <Typography
               sx={{
-                fontSize: { xs: "1rem", sm: "1.2rem", md: "1.5rem" },
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1.1rem",
+                  md: "1.2rem",
+                  lg: "1.2rem",
+                },
                 marginTop: "10px",
                 fontWeight: "500",
-                color: "white",
+                padding: { xs: "10px", sm: "10px", md: "0px" },
+                color:"white"
               }}
             >
-              Building Tomorrow’s Experts, Today
+             Building Tomorrow’s Experts, Today 
+
             </Typography>
           </Grid>
+          <Grid item xs={12} sm={12} lg={6} md={6}></Grid>
         </Grid>
-      </Banner>
+      </Box>
       <Container>
         <ContentSection>
           <Typography
@@ -176,7 +187,7 @@ const About = () => {
           </Typography>
 
           <Box textAlign="center" mt={4}>
-            <Typography variant="h6" marginBottom="5%" marginTop="10%">At Engimate, we believe that the right education can transform
+            <Typography variant="h6" marginBottom="5%" marginTop="10%" sx={{  fontSize: { lg: "1.1rem", xs: "1.0rem", md: "0.7rem" },}}>At Engimate, we believe that the right education can transform
             lives. To enhance your skills, advance your career,<br/> and contribute
             to the technology and engineering advancements of the future.</Typography>
             <Button
